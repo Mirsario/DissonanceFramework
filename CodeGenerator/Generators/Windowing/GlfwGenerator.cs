e36401a367cefe8da4a28c47ef8b8d68fd38f3b1
Mirsario <me@mirsar.io>
2021-09-12 20:00:53 +0300

    Many input-related GLFW enums.

--- CodeGenerator/Generators/Windowing/GlfwGenerator.cs
+++ CodeGenerator/Generators/Windowing/GlfwGenerator.cs
@@ -24,11 +24,22 @@ namespace CodeGenerator.Generators.Windowing
 			static string EnumItemRenamer(string name)
 				=> StringUtils.SnakeCaseToUpperCamelCase(name.Replace("GLFW_", null)).Replace("Opengl", "OpenGL");
 
+			static string PrefixNumbers(string str, string prefix)
+			{
+				if (int.TryParse(str, out _)) {
+					str = $"{prefix}{str}";
+				}
+
+				return str;
+			}
+
 			const string WindowHintEnum = "WindowHint";
 
+			// language=regex
 			MacroPlugin.Rules.AddRange(new MacroRule[] {
 				// Macros to enums
 
+				// Window Hints
 				new MacroToEnumRule(@"GLFW_(NOT_INITIALIZED|NO_CURRENT_CONTEXT|INVALID_ENUM|INVALID_VALUE|OUT_OF_MEMORY|API_UNAVAILABLE|VERSION_UNAVAILABLE|PLATFORM_ERROR|FORMAT_UNAVAILABLE)$", "GlfwError", EnumItemRenamer),
 				// Window related hints
 				new MacroToEnumRule(@"GLFW_(RESIZABLE|VISIBLE|DECORATED|FOCUSED|AUTO_ICONIFY|FLOATING|MAXIMIZED|CENTER_CURSOR|TRANSPARENT_FRAMEBUFFER|FOCUS_ON_SHOW|SCALE_TO_MONITOR)$", WindowHintEnum, EnumItemRenamer),
@@ -44,21 +55,13 @@ namespace CodeGenerator.Generators.Windowing
 				new MacroToEnumRule(@"GLFW_(COCOA_(?:RETINA_FRAMEBUFFER|FRAME_NAME|GRAPHICS_SWITCHING))$", WindowHintEnum, EnumItemRenamer),
 				// X11 specific window hints
 				new MacroToEnumRule(@"GLFW_(X11_(?:CLASS_NAME|INSTANCE_NAME))$", WindowHintEnum, EnumItemRenamer),
-
-				// Keys
-				new MacroToEnumRule(
-					@"GLFW_KEY_(.+)$",
-					"Keys",
-					s => {
-						s = s.Replace("GLFW_KEY_", null);
-
-						if (int.TryParse(s, out _)) {
-							s = $"Number{s}";
-						}
-
-						return EnumItemRenamer(s);
-					}
-				),
+				
+				// Input
+				new MacroToEnumRule(@"GLFW_GAMEPAD_BUTTON_(.+)$", "GamepadButton", s => EnumItemRenamer(s.Replace("GLFW_GAMEPAD_BUTTON_", null))),
+				new MacroToEnumRule(@"GLFW_JOYSTICK_([\dA-Z]+)$", "Joystick", s => EnumItemRenamer(PrefixNumbers(s.Replace("GLFW_JOYSTICK_", null), "Joystick"))),
+				new MacroToEnumRule(@"GLFW_MOD_(.+)$", "KeyModifier", s => EnumItemRenamer(s.Replace("GLFW_MOD_", null))),
+				new MacroToEnumRule(@"GLFW_KEY_(.+)$", "Keys", s => EnumItemRenamer(PrefixNumbers(s.Replace("GLFW_KEY_", null), "Number"))),
+				new MacroToEnumRule(@"GLFW_MOUSE_BUTTON_(.+)$", "MouseButton", s => EnumItemRenamer(PrefixNumbers(s.Replace("GLFW_MOUSE_BUTTON_", null), "Button"))),
 
 				// Macros to constants
 
